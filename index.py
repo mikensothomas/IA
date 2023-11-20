830608bbc19c98f8f87aef064bdb50e851a0f7f7
Mikenson <thomasmikenson40@gmail.com>
2023-11-20 14:20:51 -0300

    c

--- index.py
+++ index.py
@@ -1,11 +1,30 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
 
 file_path = 'archive02/wine_dataset.csv'
-dado = pd.read_csv(file_path)
-dado['style'] = dado['style'].replace('red', 1)
-dado['style'] = dado['style'].replace('white', 0)
-print(dado.head())
+dados = pd.read_csv(file_path)
+dados['style'] = dados['style'].replace('red', 1)
+dados['style'] = dados['style'].replace('white', 0)
+#print(dados.head())
+#print(dado)
+
+x = dados['style']
+y = dados.drop('style', axis = 1)
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, teste_size = 0.3)
+modelo = ExtraTreesClassifier()
+modelo.fit(x_treino, y_treino)
+
+resultado = modelo.score(x_teste, y_teste)
+print("Acuria:", resultado)
+
+print(dados.shape, x_treino.shape, x_teste.shape, y_treino.shape, y_teste.shape)
+
+print(y_teste[400:405])
+
+previsao = modelo.predict(x_teste[400:405])
+print(previsao)
