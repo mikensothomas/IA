b7c3057cbbdb35da551981390eb395a187f5e881
Mikenson <thomasmikenson40@gmail.com>
2023-11-22 01:47:53 -0300

    c

--- index.py
+++ index.py
@@ -7,8 +7,8 @@ file_path = 'archive02/wine_dataset.csv'
 dados = pd.read_csv(file_path)
 dados['style'] = dados['style'].replace('red', 1)
 dados['style'] = dados['style'].replace('white', 0)
-#print(dados.head())
-#print(dados)
+print(dados.head())
+print(dados)
 
 y = dados['style']
 x = dados.drop('style', axis = 1)
@@ -26,3 +26,14 @@ print(y_teste[400:405])
 
 previsao = modelo.predict(x_teste[400:405])
 print(previsao)
+
+"""
+x = dados[["fixed_acidity","volatile_acidity",
+       "citric_acid","residual_sugar","chlorides",
+       "free_sulfur_dioxide","total_sulfur_dioxide",
+       "density","pH","sulphates","alcohol","quality"]]
+
+y = dados["style"]
+print(x.head())
+print(y.head())
+"""
